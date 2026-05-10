cd0e660b3831048e8f96d917d8e747fab0ea5a4b
Aditya <adityasorathiya.4112@gmail.com>
2026-05-10 16:06:32 +0530

    Update api.py

--- pastedb/api.py
+++ pastedb/api.py
@@ -1,1 +1,18 @@
+import requests
 
+BASE_URL = "https://pastedb.onrender.com"
+
+def makePaste(title, content, api_key):
+
+    res = requests.post(
+        f"{BASE_URL}/api/make-paste",
+        headers={
+            "x-api-key": api_key
+        },
+        json={
+            "title": title,
+            "content": content
+        }
+    )
+
+    return res.json()
