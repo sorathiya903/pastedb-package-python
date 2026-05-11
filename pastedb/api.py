9b418ddd0abb4d844530930b787b21672bdd21e2
Aditya <adityasorathiya.4112@gmail.com>
2026-05-11 13:18:03 +0530

    Update api.py

--- pastedb/api.py
+++ pastedb/api.py
@@ -1,11 +1,8 @@
 import requests
 
 class Client:
-    def __init__(self, api_key):
-        self.api_key = api
+    def __init__(self, api_key): # Fixed argument name
+        self.api_key = api_key   # Match the argument above
 
     def test(self):
         return self.api_key
-
-
-    
